e4fb9ed2aadf23917d8f1cce5792b55278766fae
minimalparts <aurelie.herbelot@cantab.net>
2023-07-05 21:33:31 +0200

    Stopped trying to parse non txt/html files.

--- app/indexer/spider.py
+++ app/indexer/spider.py
@@ -6,7 +6,7 @@ import xmltodict
 import requests
 from collections import OrderedDict
 from os.path import join, dirname, realpath
-from app.indexer.htmlparser import extract_html
+from app.indexer.htmlparser import extract_html, auth_token
 
 dir_path = dirname(dirname(realpath(__file__)))
 
@@ -15,69 +15,68 @@ def omd_parse(current_url):
     print("\n\nRunning OMD parse on", current_url)
     links = []
     fout = open(join(dir_path,'docs_to_index.txt'),'a')
-
-    xml = requests.get(current_url, stream =True).raw
-    parse = xmltodict.parse(xml.read())
+    try:
+        xml = requests.get(current_url, timeout=10, headers={'Authorization': auth_token}, stream =True).raw
+        parse = xmltodict.parse(xml.read())
+    except:
+        print("Request failed. Moving on.")
+        return links
     docs = parse['omd_index']['doc']
-    print("PARSE:",parse, type(docs))
+    print("PARSE:",parse)
     if type(docs) is not list:
         docs = [docs]
-    print(docs)
     for doc in docs:
         urldir = '/'.join(current_url.split('/')[:-1])
 
         # URL
-        #try:
         if doc['@url'][0] == '/':
             url = doc['@url'][1:]
         else:
             url = doc['@url']
         url = join(urldir, url)
-        print("# DOC URL", url)
+        print("# DOC URL:", url)
         #if url[-1] == '/': #For local test only
         #    url = join(url,'index.html')
-        links.append(url)
-        #except:
-        #    print("\t--- No valid url")
-        #    continue
 
         # CONTENT TYPE
         try:
-            print("# DOC CONTENTTYPE", urldir, doc['@contentType'])
+            print("# DOC CONTENTTYPE: ", doc['@contentType'])
             content_type = doc['@contentType']
-            if content_type == 'folder':
+            if content_type in ['folder','desktop']: #Folder / desktop won't have any other info, so continue
+                links.append(url)
                 continue
         except:
-            print("\t--- No contentType")
+            print(" DOC CONTENTTYPE: No contentType")
+
 
         # TITLE
         try:
-            print("# DOC TITLE", urldir, doc['title'])
+            print("# DOC TITLE:", doc['title'])
             title = doc['title']
         except:
-            print("\t--- No title")
+            print("# DOC TITLE: No title")
         if title == None:
             title = ''
-        print("<doc title='"+title+"' url='"+url+"'>\n")
+        #print("<doc title='"+title+"' url='"+url+"'>\n")
         fout.write("<doc title='"+title+"' url='"+url+"'>\n")
 
         # DESCRIPTION
         try:
-            print("# DOC DESCRIPTION", urldir, doc['description'])
+            print("# DOC DESCRIPTION:", doc['description'][:100])
             description = doc['description']
             print("\t"+description+"\n")
             fout.write("\t"+description+"\n")
         except:
-            print("\t--- No description")
+            print("# DOC DESCRIPTION: No description")
     
         # CONTENT
-        #try:
-        title, body_str, snippet, cc, error = extract_html(url)
-        if not error:
-            print("# BODY", body_str)
-            fout.write(body_str+"\n")
-        #except:
-        #    print("\t--- Failed to extract text")
+        if content_type in ['text/plain','text/html']:
+            title, body_str, snippet, cc, error = extract_html(url)
+            if not error:
+                print("# DOC BODY:", body_str[:100])
+                fout.write(body_str+"\n")
+        else:
+            print("# DOC BODY: Skipping request: content is neither text/plain nor text/html.")
 
         fout.write("</doc>\n")
     fout.close()
@@ -108,11 +107,11 @@ def write_docs(base_url):
             print("\n\n#### Scraping:", url)
             links = omd_parse(url)
             for link in links:
-                print(link,pages_visited)
-                print(link,pages_to_visit)
-                print(link,urldir)
+                #print(link,pages_visited)
+                #print(link,pages_to_visit)
+                #print(link,urldir)
                 if link not in pages_visited and link not in pages_to_visit and '#' not in link: # and urldir in link:
-                    print("Found href:",link)
+                    #print("Found href:",link)
                     pages_to_visit.append(link)
         except:
             print(">> ERROR: Failed visiting current url!")
