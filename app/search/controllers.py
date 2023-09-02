46ea78a7964bc35414d7c2f767e4e321c0d393c3
minimalparts <aurelie.herbelot@cantab.net>
2023-09-02 22:31:05 +0200

    First implementation of authorization

--- app/search/controllers.py
+++ app/search/controllers.py
@@ -3,8 +3,10 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 # Import flask dependencies
-from flask import Blueprint, request, render_template, send_from_directory
+from flask import Blueprint, request, render_template, send_from_directory, make_response
 from flask import current_app
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 
 # Import the database object from the main app module
 from app import app
@@ -17,6 +19,7 @@ from scipy import sparse
 
 # Import utilities
 import re
+import requests
 import logging
 from os.path import dirname, join, realpath, isfile
 from flask import jsonify, Response
@@ -30,23 +33,21 @@ search = Blueprint('search', __name__, url_prefix='')
 dir_path = dirname(dirname(dirname(realpath(__file__))))
 pod_dir = join(dir_path,'app','static','pods')
 
-@search.route('/')
-@search.route('/index')
-def index():  
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
+    submit = SubmitField('Submit')
+
+@search.route('/user', methods=['POST','GET'])
+def user():  
     results = []
-    internal_message = ""
     if Urls.query.count() == 0:
-        internal_message = "Hey there! It looks like you're here\
-         for the first time :) To understand how to use PeARS,\
-         go to the FAQ (link at the top of the page)."
         init_podsum()
 
     query = request.args.get('q')
     if not query:
         LOG.info("No query")
-        return render_template(
-            "search/index.html",
-            internal_message=internal_message)
+        return render_template("search/user.html")
     else:
         results = []
         query = query.lower()
@@ -58,6 +59,65 @@ def index():
         return r
 
 
-@search.route('/html_cache/<path:filename>')
-def custom_static(filename):
-    return send_from_directory('html_cache', filename)
+@search.route('/anonymous', methods=['POST','GET'])
+def anonymous():  
+    results = []
+    if Urls.query.count() == 0:
+        init_podsum()
+
+    query = request.args.get('q')
+    if not query:
+        LOG.info("No query")
+        return render_template("search/anonymous.html")
+    else:
+        results = []
+        query = query.lower()
+        pears = ['0.0.0.0']
+        results, pods = score_pages.run(query, pears)
+        print(results)
+        r = app.make_response(jsonify(results))
+        r.mimetype = "application/json"
+        return r
+
+
+
+@search.route('/', methods=['GET','POST'])
+@search.route('/index', methods=['GET','POST'])
+def index():
+    access_token = request.cookies.get('OMD_SESSION_ID')  
+    if not access_token:
+        return render_template('search/anonymous.html')
+    else:
+        url = 'http://localhost:9191/api' #TODO: change URL to OMD endpoint
+        data = {'action': 'getUserInfo', 'session_id': access_token}
+        resp = requests.post(url, data=data)
+        username = resp.json()['username']
+        return render_template('search/user.html', username=username)
+
+
+
+@search.route('/login', methods=['GET', 'POST'])
+def login():
+    # Declare the login form using FlaskForm library
+    form = LoginForm(request.form)
+    # Flask message injected into the page, in case of any errors
+    msg = None
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str)
+        # send authorization message to on my disk
+        url = 'http://localhost:9191/api' #TODO: change URL to OMD endpoint
+        data = {'action': 'signin', 'username': username, 'password': password}
+        resp = requests.post(url, data=data)
+        access_token = resp.cookies.get('OMD_SESSION_ID')
+        print(resp.json())
+        data = {'action': 'getUserInfo', 'session_id': access_token}
+        resp = requests.post(url, data=data)
+        username = resp.json()['username']
+        return render_template('search/user.html', welcome="Welcome "+username)
+    else:
+       msg = "Unknown user"
+       return render_template( 'search/login.html', form=form, msg=msg)
+
