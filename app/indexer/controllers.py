Fixed dimensionality in overlap calculation

app/indexer/controllers.py
@@ -114,7 +114,7 @@ def progress_crawl():
             init_podsum()
         if not isfile(join(pod_dir,pod_name)):
             print("Making 0 CSR matrix")
-            pod = np.zeros((1,8000))
+            pod = np.zeros((1,16000))
             pod = sparse.csr_matrix(pod)
             sparse.save_npz(join(pod_dir,pod_name), pod)
 

app/pod_finder/controllers.py
@@ -232,10 +232,10 @@ def unsubscribe():
             pod_entry.registered = False
             db.session.commit()
             print("Reverting CSR matrix to 0")
-            pod = np.zeros((1,10000))
+            pod = np.zeros((1,16000))
             pod = csr_matrix(pod)
             save_npz(join(pod_dir,pod_name+".npz"), pod)
             print("Reverting summary to 0")
-            pod_from_file(pod_name, lang, np.zeros(10000))
+            pod_from_file(pod_name, lang, np.zeros(16000))
     return render_template(
         'pod_finder/unsubscribe-success.html', pods=unsubscribed_pods)

app/search/overlap_calculation.py
@@ -36,7 +36,7 @@ def generic_overlap(i1, i2):
     return dice(set(words1), set(words2))
 
 def completeness(v, m):
-    v = v.reshape(10000,)
+    v = v.reshape(16000,)
     idx = np.where(v != 0)
     v_nz = v[idx]
     numcols = v_nz.shape[0]

app/utils.py
@@ -117,7 +117,7 @@ def init_podsum():
     Path(pod_dir).mkdir(exist_ok=True, parents=True)
     print("Making 0 CSR matrix for pod summaries")
     print("POD DIR",pod_dir)
-    pod_summaries = np.zeros((1,8000))
+    pod_summaries = np.zeros((1,16000))
     pod_summaries = csr_matrix(pod_summaries)
     save_npz(join(pod_dir,"podsum.npz"), pod_summaries)
 

app/utils_db.py
@@ -62,7 +62,7 @@ def get_db_pod_language(url):
 
 def compute_pod_summary(name):
     '''This function is very similar to 'self' in PeARS-pod'''
-    DS_vector = np.zeros(8000) 
+    DS_vector = np.zeros(16000) 
     #DS_vector = np.zeros(256) 
     for u in db.session.query(Urls).filter_by(pod=name).all():
         DS_vector += convert_to_array(u.vector)