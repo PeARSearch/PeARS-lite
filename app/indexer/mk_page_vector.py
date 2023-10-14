55066db8eb92815883f4f6ea555797ba02266678
minimalparts <aurelie.herbelot@cantab.net>
2023-10-14 13:00:20 +0200

    Added try statement to indexer

--- app/indexer/mk_page_vector.py
+++ app/indexer/mk_page_vector.py
@@ -39,22 +39,26 @@ def compute_vectors(target_url, keyword, lang):
         u = Urls(url=target_url)
         title, body_str, snippet, cc, error = extract_html(target_url)
         if error is None and snippet != '':
-            text = title + " " + body_str
-            text = tokenize_text(lang, text)
-            pod_m = compute_vec(lang, text, pod_m)
-            u.title = str(title)
-            u.vector = str(pod_m.shape[0]-1)
-            u.keyword = keyword
-            u.pod = keyword
-            u.snippet = str(snippet)
-            if cc:
-                u.cc = True
-            #print(u.url,u.title,u.vector,u.snippet,u.cc,u.pod)
-            db.session.add(u)
-            db.session.commit()
-            save_npz(join(pod_dir,keyword+'.npz'),pod_m)
-            podsum = np.sum(pod_m, axis=0)
-            return True, podsum
+            try:
+                text = title + " " + body_str
+                text = tokenize_text(lang, text)
+                pod_m = compute_vec(lang, text, pod_m)
+                u.title = str(title)
+                u.vector = str(pod_m.shape[0]-1)
+                u.keyword = keyword
+                u.pod = keyword
+                u.snippet = str(snippet)
+                if cc:
+                    u.cc = True
+                #print(u.url,u.title,u.vector,u.snippet,u.cc,u.pod)
+                db.session.add(u)
+                db.session.commit()
+                save_npz(join(pod_dir,keyword+'.npz'),pod_m)
+                podsum = np.sum(pod_m, axis=0)
+                return True, podsum
+            except:
+                print("ERROR: try statement failed while indexing url",target_url)
+                return False, None
         else:
             if snippet == '':
                 print("IGNORING URL: Snippet empty.")
