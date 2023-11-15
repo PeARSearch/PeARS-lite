602ae4ac0d15e01da35872619a05fb3ba5eac439
minimalparts <aurelie.herbelot@cantab.net>
2023-11-15 12:42:58 +0100

    Added alternative init file for pythonanywhere use

---
+++ app/__init_for_pythonanywhere__.py
@@ -0,0 +1,121 @@
+# SPDX-FileCopyrightText: 2023 PeARS Project, <community@pearsproject.org> 
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+import os
+import logging
+
+# Import flask and template operators
+from flask import Flask, render_template
+from flask_admin import Admin
+
+# Import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+# Get paths to SentencePiece model and vocab
+LANG = 'en' # hardcoded for now
+SPM_DEFAULT_VOCAB_PATH = f'/home/<your_username>/PeARS-lite/app/api/models/{LANG}/{LANG}wiki.lite.16k.vocab'
+spm_vocab_path = os.environ.get("SPM_VOCAB", SPM_DEFAULT_VOCAB_PATH)
+SPM_DEFAULT_MODEL_PATH = f'/home/<your_username>/PeARS-lite/app/api/models/{LANG}/{LANG}wiki.lite.16k.model'
+spm_model_path = os.environ.get("SPM_MODEL", SPM_DEFAULT_MODEL_PATH)
+
+# Define vector size
+#from app.indexer.vectorizer import read_vocab
+from app.readers import read_vocab
+from sklearn.feature_extraction.text import CountVectorizer
+
+print(f"Loading SPM vocab from '{spm_vocab_path}' ...")
+vocab, inverted_vocab, logprobs = read_vocab(spm_vocab_path)
+vectorizer = CountVectorizer(vocabulary=vocab, lowercase=True, token_pattern='[^ ]+')
+VEC_SIZE = len(vocab)
+
+
+def configure_logging():
+    # register root logging
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('werkzeug').setLevel(logging.INFO)
+
+
+configure_logging()
+
+# Define the WSGI application object
+app = Flask(__name__)
+
+# Configurations
+#app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/<your_username>/PeARS-lite/app.db'
+
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
+
+
+# Import a module / component using its blueprint handler variable (mod_auth)
+from app.indexer.controllers import indexer as indexer_module
+from app.api.controllers import api as api_module
+from app.search.controllers import search as search_module
+from app.pod_finder.controllers import pod_finder as pod_finder_module
+from app.orchard.controllers import orchard as orchard_module
+from app.pages.controllers import pages as pages_module
+from app.settings.controllers import settings as settings_module
+
+# Register blueprint(s)
+app.register_blueprint(indexer_module)
+app.register_blueprint(api_module)
+app.register_blueprint(search_module)
+app.register_blueprint(pod_finder_module)
+app.register_blueprint(orchard_module)
+app.register_blueprint(pages_module)
+app.register_blueprint(settings_module)
+# ..
+
+# Build the database:
+# This will create the database file using SQLAlchemy
+# db.drop_all()
+with app.app_context():
+    db.create_all()
+
+from flask_admin.contrib.sqla import ModelView
+from app.api.models import Pods, Urls
+from app.api.controllers import return_url_delete, return_pod_delete
+
+from flask_admin import expose
+from flask_admin.contrib.sqla.view import ModelView
+from flask_admin.model.template import EndpointLinkRowAction
+
+# Flask and Flask-SQLAlchemy initialization here
+
+admin = Admin(app, name='PeARS DB', template_mode='bootstrap3')
+
+class UrlsModelView(ModelView):
+    list_template = 'admin/pears_list.html'
+    column_exclude_list = ['vector','snippet']
+    column_searchable_list = ['url', 'title', 'doctype', 'notes', 'pod']
+    column_editable_list = ['notes']
+    can_edit = True
+    page_size = 100
+    form_widget_args = {
+        'vector': {
+            'readonly': True
+        },
+        'url': {
+            'readonly': True
+        },
+        'pod': {
+            'readonly': True
+        },
+        'date_created': {
+            'readonly': True
+        },
+        'date_modified': {
+            'readonly': True
+        },
+    }
+    def delete_model(self, model):
+        try:
+            self.on_model_delete(model)
+            print("DELETING",model.url,model.vector)
+            # Add your custom logic here and don't forget to commit any changes e.g.
+            print(return_url_delete(model.url))
+            self.session.commit()
+        except Exception as ex:
