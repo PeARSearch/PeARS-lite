a21b167c8f2ee0a66f9b86145d268ae8356eee14
tlmnhut <tlmnhut2207@gmail.com>
2023-12-03 23:37:47 +0100

    add more datasets

--- run_projection_experiments.bash
+++ run_projection_experiments.bash
@@ -10,11 +10,11 @@ export SPM_MODEL="./app/api/models/en/enwiki.model"
 
 PROJECTION_OPTIONS=(
 
-    ../datasets/projection_experiments/proj_mat/256/ach/111.npy
-    ../datasets/projection_experiments/proj_mat/256/ach/222.npy
-    ../datasets/projection_experiments/proj_mat/256/ach/333.npy
-    ../datasets/projection_experiments/proj_mat/256/ach/444.npy
-    ../datasets/projection_experiments/proj_mat/256/ach/555.npy
+    ../datasets/projection_experiments/proj_mat/256/fasttext_min10/111.npy
+    ../datasets/projection_experiments/proj_mat/256/fasttext_min10/222.npy
+    ../datasets/projection_experiments/proj_mat/256/fasttext_min10/333.npy
+    ../datasets/projection_experiments/proj_mat/256/fasttext_min10/444.npy
+    ../datasets/projection_experiments/proj_mat/256/fasttext_min10/555.npy
 )
 
 for option in "${PROJECTION_OPTIONS[@]}"
@@ -49,34 +49,98 @@ do
 
 done
 
-for option in "${PROJECTION_OPTIONS[@]}"
-do
-    # clear up index and db
-    rm -f app/static/db/app.db
-    rm -fr app/static/pods/*npz
+#for option in "${PROJECTION_OPTIONS[@]}"
+#do
+#    # clear up index and db
+#    rm -f app/static/db/app.db
+#    rm -fr app/static/pods/*npz
+#
+#    # start the server, wait until it's running
+#    export PROJ_PATH=$option
+#    { python run.py & } &> _run_logs_accountant.txt
+#    sleep 5
+#
+#    pushd evaluate/
+#
+#    echo "-------------"
+#    echo "projection path: $option"
+#
+#    echo "indexing: 1-accountant"
+#    curl -# "localhost:9090/indexer/from_crawl?url=http://localhost:9090/static/testdocs/1_accountant/index.html&keyword=1_accountant" > /dev/null
+#
+#    echo "evaluating: 1-accountant"
+#    python eval.py 1_accountant --pod 1_accountant --query_dir $PERSONAS_DIR/personas_queries/
+#
+#    echo "-------------"
+#    echo
+#
+#    popd
+#
+#    # stop the server
+#    kill %1  # kill last process
+#
+#done
 
-    # start the server, wait until it's running
-    export PROJ_PATH=$option
-    { python run.py & } &> _run_logs_accountant.txt
-    sleep 5
-
-    pushd evaluate/
-
-    echo "-------------"
-    echo "projection path: $option"
-
-    echo "indexing: 1-accountant"
-    curl -# "localhost:9090/indexer/from_crawl?url=http://localhost:9090/static/testdocs/1_accountant/index.html&keyword=1_accountant" > /dev/null
-
-    echo "evaluating: 1-accountant"
-    python eval.py 1_accountant --pod 1_accountant --query_dir $PERSONAS_DIR/personas_queries/
-
-    echo "-------------"
-    echo
-
-    popd
-
-    # stop the server
-    kill %1  # kill last process
-
-done
+#for option in "${PROJECTION_OPTIONS[@]}"
+#do
+#    # clear up index and db
+#    rm -f app/static/db/app.db
+#    rm -fr app/static/pods/*npz
+#
+#    # start the server, wait until it's running
+#    export PROJ_PATH=$option
+#    { python run.py & } &> /dev/null
+#    sleep 5
+#
+#    pushd evaluate/
+#
+#    echo "-------------"
+#    echo "projection path: $option"
+#
+#    echo "indexing: 2_news"
+#    curl -# "localhost:9090/indexer/from_crawl?url=http://localhost:9090/static/testdocs/2_news/index.html&keyword=0_hr" > /dev/null
+#
+#    echo "evaluating: 2_news"
+#    python eval.py 2_news --pod 2_news --query_dir $PERSONAS_DIR/personas_queries/
+#
+#    echo "-------------"
+#    echo
+#
+#    popd
+#
+#    # stop the server
+#    kill %1  # kill last process
+#
+#done
+#
+#for option in "${PROJECTION_OPTIONS[@]}"
+#do
+#    # clear up index and db
+#    rm -f app/static/db/app.db
+#    rm -fr app/static/pods/*npz
+#
+#    # start the server, wait until it's running
+#    export PROJ_PATH=$option
+#    { python run.py & } &> /dev/null
+#    sleep 5
+#
+#    pushd evaluate/
+#
+#    echo "-------------"
+#    echo "projection path: $option"
+#
+#    echo "indexing: 3_moviesummary"
+#    curl -# "localhost:9090/indexer/from_crawl?url=http://localhost:9090/static/testdocs/3_moviesummary/index.html&keyword=1_accountant" > /dev/null
+#
+#    echo "evaluating: 3_moviesummary"
+#    python eval.py 3_moviesummary --pod 3_moviesummary --query_dir $PERSONAS_DIR/personas_queries/
+#
+#    echo "-------------"
+#    echo
+#
+#    popd
+#
+#    # stop the server
+#    kill %1  # kill last process
+#
+#done
